ae7d0b8c3be3c4ed59f1af9a85cf2e6373438634
andreLagacione <lagacioneandre@gmail.com>
2020-01-14 22:45:57 -0300

    change name main file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+var path = require('path');
 const app = express();
 const port = process.env.port || 3001;
 const corsOptions = {
@@ -11,7 +12,10 @@ const corsOptions = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', require('./router'));
+
 app.listen(port);
 
 console.log('Application running in port ' + port);
