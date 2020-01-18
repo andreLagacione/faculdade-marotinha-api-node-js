10cc47a6b71bbafe2a6d7e86f7c87e70e0ae2791
andreLagacione <lagacioneandre@gmail.com>
2020-01-18 15:54:29 -0300

    pagination improvement

--- commons/pagination/index.js
+++ commons/pagination/index.js
@@ -1,16 +1,22 @@
-exports.pagination = (pageNumber, pageSize, totalElements) => {
-    const totalPages = Math.round(totalElements / pageSize);
+exports.pagination = (pageNumber = 0, pageSize = 25, elements) => {
+    console.log(pageNumber, pageSize);
+    const totalElements = elements.length;
+    const totalPages = Math.ceil(totalElements / pageSize);
     const isFirstPage = pageNumber === 0 ? true : false;
     const isLastPage = pageNumber < totalPages ? false : true;
+    const initGetData = pageNumber === 0 ? 0 : pageNumber * pageSize + 1;
+    const endGetData = pageNumber === 0 ? pageSize : initGetData + 25 - 1;
+    const elementsInPage = elements.splice(initGetData, endGetData);
+
 
     return {
-        content: [],
+        content: elementsInPage,
         totalElements: totalElements,
-        totalPages: totalPages,
+        totalPages: totalPages || 1,
         last: isLastPage,
         first: isFirstPage,
-        size: pageSize || 25,
-        number: 0,
+        size: pageSize,
+        number: pageNumber,
         numberOfElements: pageSize || 25
     }
 }
