e453c99edf8173fa9fdff4b7450c7d5017cb2c8c
andreLagacione <lagacioneandre@gmail.com>
2020-01-18 15:57:37 -0300

    using pagination class

--- materias/index.js
+++ materias/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../mongo-config');
 const schema = require('./schema');
+const { pagination } = require('../commons/pagination');
 
 
 router.get('/', (request, response) => {
@@ -10,8 +11,12 @@ router.get('/', (request, response) => {
 
     model.find({}).lean().exec(
         (_error, _response) => {
-            console.log('getList ' + _error, _response);
-            response.json(_response);
+            if (_error) {
+                return _error;
+            }
+
+            let _pagination = pagination(pageNumber, pageSize, _response)
+            response.json(_pagination);
         }
     );
 });
