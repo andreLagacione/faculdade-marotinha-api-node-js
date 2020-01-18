2b1d406985d674955a4a45454cd66fa56c4b3864
andreLagacione <lagacioneandre@gmail.com>
2020-01-18 16:28:32 -0300

    improvement pagination to get last page carrectly

--- commons/pagination/index.js
+++ commons/pagination/index.js
@@ -1,11 +1,20 @@
 exports.pagination = (pageNumber = 0, pageSize = 25, elements) => {
-    console.log(pageNumber, pageSize);
     const totalElements = elements.length;
     const totalPages = Math.ceil(totalElements / pageSize);
     const isFirstPage = pageNumber === 0 ? true : false;
     const isLastPage = pageNumber < totalPages ? false : true;
-    const initGetData = pageNumber === 0 ? 0 : pageNumber * pageSize + 1;
-    const endGetData = pageNumber === 0 ? pageSize : initGetData + 25 - 1;
+    let initGetData = 0;
+    let endGetData = pageSize;
+
+    if (pageNumber > 0 && !isLastPage) {
+        initGetData = pageNumber * pageSize + 1;
+    }
+
+    if (isLastPage) {
+        initGetData = pageNumber * pageSize - pageSize;
+        endGetData = elements.length;
+    }
+
     const elementsInPage = elements.splice(initGetData, endGetData);
 
     return {
