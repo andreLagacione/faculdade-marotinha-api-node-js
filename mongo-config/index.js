a5c2817af6cd1642ef8df9be79bf7b07bc64f1ab
andreLagacione <lagacioneandre@gmail.com>
2020-01-19 14:54:06 -0300

    set configs to mongoose conections

--- mongo-config/index.js
+++ mongo-config/index.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/faculdade_marotinha');
+mongoose.connect(
+    'mongodb://localhost:27017/faculdade_marotinha',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 module.exports = mongoose;
