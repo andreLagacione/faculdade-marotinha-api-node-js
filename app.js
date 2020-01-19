286407abd56fdb0e5f97b2701f5bec9ac01ce26f
andreLagacione <lagacioneandre@gmail.com>
2020-01-19 14:55:42 -0300

    change error function

--- app.js
+++ app.js
@@ -24,10 +24,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: err
-    });
+    res.end('Cannot ' + req.method + ' ' + req.url);
+    next(err);
 });
 
 console.log('Application running in port ' + port);
