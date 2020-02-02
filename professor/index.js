ef7ce3e119d474de6a7c947f66909ad1ccdedf8c
andreLagacione <lagacioneandre@gmail.com>
2020-02-02 13:44:14 -0300

    create map to get professor by id

--- professor/index.js
+++ professor/index.js
@@ -4,4 +4,6 @@ const professorController = require('./controller/professorController');
 router.post('/list', professorController.index);
 router.post('/', professorController.store);
 
+router.get('/:id', professorController.show);
+
 module.exports = router;
