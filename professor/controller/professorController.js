4490415f57257dfd5e383f095cf6a5690bd0255f
andreLagacione <lagacioneandre@gmail.com>
2020-02-02 14:51:02 -0300

    create method to update professor

--- professor/controller/professorController.js
+++ professor/controller/professorController.js
@@ -5,7 +5,7 @@ const model = db.model('professores', schema, 'professores', true);
 const materiaModel = db.model('materias');
 const ObjectId = require('mongodb').ObjectID;
 const { convertId, structureArrayOfObjectId } = require('../../commons/convert-id');
-const checkCpf = require('../../commons/cpf');
+const { checkCpf, findRegisterByCpf } = require('../../commons/cpf');
 
 module.exports = {
     async index(request, response) {
@@ -31,7 +31,7 @@ module.exports = {
 
     async store(request, response) {
         const cpf = request.body.cpf;
-        const _checkCpf = await checkCpf(cpf, model);
+        const _checkCpf = await checkCpf(cpf, model, true);
 
         if (_checkCpf) {
             response.status(_checkCpf.httpStatusCode).send(_checkCpf);
@@ -82,6 +82,60 @@ module.exports = {
             httpStatus: 'Not Found',
             httpStatusCode: 404,
             message: 'Professor(a) não encontrado(a)!'
+        });
+    },
+
+    async update(request, response) {
+        const cpf = request.body.cpf;
+        let professorId = request.body.id;
+        const findProfessor = await findRegisterByCpf(cpf, model);
+        let idProfessorFound;
+        
+        if (findProfessor.length) {
+            idProfessorFound = findProfessor[0]._id.toString();
+        }
+
+        if (idProfessorFound !== professorId) {
+            response.status(405).send({
+                httpStatus: 'Method Not Allowed',
+                httpStatusCode: 405,
+                message: 'Já existe um registro cadastrado com esse CPF. Por favor informe outro CPF!'
+            });
+        }
+
+        const _checkCpf = await checkCpf(cpf, model, false);
+
+        if (_checkCpf) {
+            response.status(_checkCpf.httpStatusCode).send(_checkCpf);
+            return false;
+        }
+
+        const _response = await model.updateOne({
+            _id: professorId
+        }, {
+                $set: {
+                    name: request.body.name,
+                    age: request.body.age,
+                    cpf: cpf,
+                    phone: request.body.phone,
+                    materias: request.body.materias
+            }
+        });
+
+        if (_response && _response.nModified === 0) {
+            response.status(404).send({
+                httpStatus: 'Not Found',
+                httpStatusCode: 404,
+                message: 'Professor(a) não encontrado(a)!'
+            });
+
+            return false;
+        }
+
+        response.json({
+            httpStatus: 'OK',
+            httpStatusCode: 200,
+            message: 'Professor(a) alterado(a) com sucesso!'
         });
     },
 };
