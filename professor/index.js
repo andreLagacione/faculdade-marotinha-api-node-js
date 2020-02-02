0767b0f0c1ceb1c514b569fb3b578af9a089c6d9
andreLagacione <lagacioneandre@gmail.com>
2020-02-01 21:18:52 -0300

    create map to save new professor

--- professor/index.js
+++ professor/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const professorController = require('./controller/professorController');
 
 router.post('/list', professorController.index);
+router.post('/', professorController.store);
 
 module.exports = router;
