ad238541050836e0af495632a6d04c2f73f36a72
andreLagacione <lagacioneandre@gmail.com>
2020-02-02 15:09:27 -0300

    create controller to return professor list for a combo box

---
+++ professor/controller/comboListController.js
@@ -0,0 +1,20 @@
+const db = require('../../mongo-config');
+const schema = require('../schema');
+const model = db.model('professores', schema, 'professores');
+const { convertId } = require('../../commons/convert-id');
+
+module.exports = {
+    async index(request, response) {
+        const professores = await model.find({}).sort([['name', 'asc']]).lean().exec();
+        let comboList = [];
+
+        professores.map(item => {
+            comboList.push({
+                name: item.name,
+                id: item._id
+            });
+        });
+
+        response.json(comboList);
+    }
+}
