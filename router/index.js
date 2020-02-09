ecbe77b4bec05009354ad961ba70813074b486da
andreLagacione <lagacioneandre@gmail.com>
2020-02-09 20:03:07 -0300

    map route for turma

--- router/index.js
+++ router/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/materia', require('../materia'));
 router.use('/professor', require('../professor'));
 router.use('/curso', require('../curso'));
+router.use('/turma', require('../turma'));
 
 module.exports = router;
