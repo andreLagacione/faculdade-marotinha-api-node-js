9c6fa881d2c91bb0f245d9e4ab3d9c2a754e7037
andreLagacione <lagacioneandre@gmail.com>
2020-02-09 15:32:57 -0300

    map route to remove course

--- curso/index.js
+++ curso/index.js
@@ -6,5 +6,6 @@ router.post('/list', cursoController.index);
 router.post('/', cursoController.store);
 router.get('/:id', cursoController.show);
 router.put('/', cursoController.update);
+router.delete('/:id', cursoController.destroy);
 
 module.exports = router;
