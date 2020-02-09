0470038754a4211204c7ee954faad7a6f3d54dd2
andreLagacione <lagacioneandre@gmail.com>
2020-02-09 13:36:21 -0300

    create global validators

---
+++ commons/validators/index.js
@@ -0,0 +1,27 @@
+exports.validateIfCourseExist = async (courseName, model) => {
+    const courses = await model.find({}).lean().exec();
+
+    for (let item of courses) {
+        if (item.name === courseName) {
+            return {
+                httpStatus: 'Method Not Allowed',
+                httpStatusCode: 405,
+                message: 'Já existe um curso cadastrado com esse nome. Por favor informe outro nome!'
+            };
+        }
+    }
+
+    return false;
+}
+
+exports.validateIfHasSubjects = async (subjectsList) => {
+    if (!subjectsList.length) {
+        return {
+            httpStatus: 'Method Not Allowed',
+            httpStatusCode: 405,
+            message: 'Informe pelo menos uma matéria!'
+        };
+    }
+
+    return false;
+}
