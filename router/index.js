6e46bbfe05911ad2fc2fa53c97f437553c02007c
andreLagacione <lagacioneandre@gmail.com>
2020-02-09 13:37:02 -0300

    add route for course

--- router/index.js
+++ router/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/materia', require('../materia'));
 router.use('/professor', require('../professor'));
+router.use('/curso', require('../curso'));
 
 module.exports = router;
