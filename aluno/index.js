c808d3f97f6c7da861f79d60a01cd1bd27c270a2
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 19:07:32 -0300

    create end point to get aluno by id

--- aluno/index.js
+++ aluno/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const alunoController = require('./controller/alunoController');
 
 router.post('/list', alunoController.index);
+router.post('/', alunoController.store);
+
+router.get('/:id', alunoController.show);
 
 
 module.exports = router;
