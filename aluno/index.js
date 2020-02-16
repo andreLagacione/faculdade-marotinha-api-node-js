94cf95e84d10378a941ee39af1fc1a96e9e1f8e7
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 19:49:22 -0300

    create end point to save aluno edited

--- aluno/index.js
+++ aluno/index.js
@@ -5,6 +5,7 @@ router.post('/list', alunoController.index);
 router.post('/', alunoController.store);
 
 router.get('/:id', alunoController.show);
+router.put('/', alunoController.update);
 
 
 module.exports = router;
