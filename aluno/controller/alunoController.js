ece16c369ac2cf1471a54e3a5ae2568dbdaafc5c
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 19:08:51 -0300

    create methods to save new aluno and get by id

--- aluno/controller/alunoController.js
+++ aluno/controller/alunoController.js
@@ -2,7 +2,7 @@ const db = require('../../mongo-config');
 const schema = require('../schema');
 const { pagination, paginationParams } = require('../../commons/pagination');
 const model = db.model('alunos', schema, 'alunos', true);
-const materiaModel = db.model('materias');
+const turmaModel = db.model('turmas');
 const ObjectId = require('mongodb').ObjectID;
 const { convertId, structureArrayOfObjectId } = require('../../commons/convert-id');
 const { checkCpf, findRegisterByCpf } = require('../../commons/cpf');
@@ -16,18 +16,93 @@ module.exports = {
         _pagination.content = alunoListDTO(_pagination.content);
         response.json(_pagination);
     },
+
+    async store(request, response) {
+        const cpf = request.body.cpf.trim();
+        const _checkCpf = await checkCpf(cpf, model, true);
+        const validateSubjects = validateIfHasSubjects(request.body.turmas, 'turma');
+
+        if (_checkCpf) {
+            response.status(_checkCpf.httpStatusCode).send(_checkCpf);
+            return false;
+        }
+
+        if (validateSubjects) {
+            response.status(validateSubjects.httpStatusCode).send(validateSubjects);
+            return false;
+        }
+
+        const newAluno = new model({
+            name: request.body.name.trim(),
+            age: request.body.age,
+            cpf: cpf,
+            phone: request.body.phone.trim(),
+            turmas: request.body.turmas
+        });
+
+        const save = await newAluno.save();
+
+        response.json({
+            httpStatus: 'OK',
+            httpStatusCode: 200,
+            message: 'Aluno(a) adicionado(a) com sucesso!'
+        });
+    },
+
+    async show(request, response) {
+        const _id = request.params.id;
+        const aluno = await model.findById(_id).lean().exec();
+        let turmasList;
+
+        if (aluno) {
+            const idTurmas = aluno.turmas;
+
+            if (idTurmas.length) {
+                const objectIdArray = structureArrayOfObjectId(idTurmas);
+                turmasList = await turmaModel.find({
+                    _id: {
+                        $in: objectIdArray
+                    }
+                }).lean().exec();
+            }
+
+            aluno.turmas = buildTurmaDTO(turmasList);
+            response.setHeader('Content-Type', 'application/json');
+            response.json(convertId([aluno])[0]);
+            return false;
+        }
+
+        response.status(404).send({
+            httpStatus: 'Not Found',
+            httpStatusCode: 404,
+            message: 'Aluno(a) não encontrado(a)!'
+        });
+    },
 };
 
-const alunoListDTO = (professorList) => {
+const alunoListDTO = (alunoList) => {
     let listDTO = [];
 
-    professorList.map(item => {
+    alunoList.map(item => {
         listDTO.push({
             name: item.name,
             age: item.age,
             cpf: item.cpf,
             phone: item.phone,
             id: item._id
+        });
+    });
+
+    return listDTO;
+}
+
+const buildTurmaDTO = (alunoList) => {
+    let listDTO = [];
+
+    alunoList.map(item => {
+        listDTO.push({
+            id: item._id,
+            name: `${item.cursoName} - período da ${item.periodo}` 
         });
     });
 
