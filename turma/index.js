7ab6a8234d15715ead7dabb1681f785d51bd23f7
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 13:33:23 -0300

    create end point to get turma to edit

--- turma/index.js
+++ turma/index.js
@@ -4,4 +4,6 @@ const turmaController = require('./controller/turmaController');
 router.post('/list', turmaController.index);
 router.post('', turmaController.store);
 
+router.get('/:id', turmaController.show);
+
 module.exports = router;
