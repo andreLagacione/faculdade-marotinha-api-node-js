8b647fa2fd139f3ccef134aec1c552792eb861f8
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 14:12:02 -0300

    create end point to combo list

--- turma/index.js
+++ turma/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const turmaController = require('./controller/turmaController');
+const comboListController = require('./controller/comboListController');
 
 router.post('/list', turmaController.index);
 router.post('', turmaController.store);
-
+router.get('/combo-list', comboListController.index);
 router.get('/:id', turmaController.show);
 router.put('/', turmaController.update);
 router.delete('/:id', turmaController.destroy);
