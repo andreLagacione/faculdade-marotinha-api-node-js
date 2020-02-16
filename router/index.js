3e420bb5932037668a08ff6450c36c64746140ec
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 15:00:53 -0300

    add route for aluno

--- router/index.js
+++ router/index.js
@@ -4,5 +4,6 @@ router.use('/materia', require('../materia'));
 router.use('/professor', require('../professor'));
 router.use('/curso', require('../curso'));
 router.use('/turma', require('../turma'));
+router.use('/aluno', require('../aluno'));
 
 module.exports = router;
