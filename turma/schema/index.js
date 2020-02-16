84dae1fd7e52d392a7f08b2a97794b1a46f3ad6d
andreLagacione <lagacioneandre@gmail.com>
2020-02-16 12:55:08 -0300

    add props

--- turma/schema/index.js
+++ turma/schema/index.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const turmaSchema = new mongoose.Schema({
     ano: String,
     curso: String,
+    cursoName: String,
     professor: String,
+    professorName: String,
     periodo: String
 }, {
     collection: 'turmas'
