5d1f77052467f49ef6468c87b692c63ee8314efa
andreLagacione <lagacioneandre@gmail.com>
2020-02-29 19:11:35 -0300

    map endpoints

---
+++ boletim/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const boletimController = require('./controller/boletimController');
+// const comboListController = require('./controller/comboListController');
+
+router.post('/list', boletimController.index);
+// router.post('/', boletimController.store);
+// router.get('/combo-list', comboListController.index);
+// router.get('/:id', boletimController.show);
+// router.put('/', boletimController.update);
+// router.delete('/:id', boletimController.destroy);
+module.exports = router;
