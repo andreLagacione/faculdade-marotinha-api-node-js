735f1746c99e02c650784db16593b5145564ef49
andreLagacione <lagacioneandre@gmail.com>
2020-03-01 17:47:42 -0300

    add route map for nota

--- router/index.js
+++ router/index.js
@@ -6,5 +6,6 @@ router.use('/curso', require('../curso'));
 router.use('/turma', require('../turma'));
 router.use('/aluno', require('../aluno'));
 router.use('/boletim', require('../boletim'));
+router.use('/nota/id-boletim', require('../nota'));
 
 module.exports = router;
