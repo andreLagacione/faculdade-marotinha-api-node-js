a0784f154a0be64e0a381390c1d4842aab491627
andreLagacione <lagacioneandre@gmail.com>
2020-03-01 16:59:18 -0300

    end point to get boletim by id

--- boletim/index.js
+++ boletim/index.js
@@ -5,7 +5,7 @@ const boletimController = require('./controller/boletimController');
 router.post('/list', boletimController.index);
 router.post('/', boletimController.store);
 // router.get('/combo-list', comboListController.index);
-// router.get('/:id', boletimController.show);
+router.get('/:id', boletimController.show);
 // router.put('/', boletimController.update);
 // router.delete('/:id', boletimController.destroy);
 module.exports = router;
