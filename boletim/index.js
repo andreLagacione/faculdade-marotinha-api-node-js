5057d3451c8f449d607ab3022c297e72774d4235
andreLagacione <lagacioneandre@gmail.com>
2020-03-01 18:10:08 -0300

    remove map for combo list

--- boletim/index.js
+++ boletim/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const boletimController = require('./controller/boletimController');
-// const comboListController = require('./controller/comboListController');
 
 router.post('/list', boletimController.index);
 router.post('/', boletimController.store);
-// router.get('/combo-list', comboListController.index);
 router.get('/:id', boletimController.show);
 router.put('/', boletimController.update);
 router.delete('/:id', boletimController.destroy);
