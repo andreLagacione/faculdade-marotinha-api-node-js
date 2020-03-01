5db0f31f239d7cf0e56cfb3ecbdf5142166f9432
andreLagacione <lagacioneandre@gmail.com>
2020-03-01 17:48:29 -0300

    map end point for edit boletim

--- boletim/index.js
+++ boletim/index.js
@@ -6,6 +6,6 @@ router.post('/list', boletimController.index);
 router.post('/', boletimController.store);
 // router.get('/combo-list', comboListController.index);
 router.get('/:id', boletimController.show);
-// router.put('/', boletimController.update);
+router.put('/', boletimController.update);
 // router.delete('/:id', boletimController.destroy);
 module.exports = router;
