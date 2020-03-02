b3e4b6e0c242c18806cc5696f49a86f4fbda77ca
andreLagacione <lagacioneandre@gmail.com>
2020-03-02 20:43:38 -0300

    create end point to get nota by id

--- nota/index.js
+++ nota/index.js
@@ -6,6 +6,7 @@ const notaController = require('./controller/notaController');
 router.post('/', notaController.store);
 // router.get('/combo-list', comboListController.index);
 router.get('/id-boletim/:id', notaController.index);
+router.get('/:id', notaController.show);
 // router.put('/', notaController.update);
 // router.delete('/:id', notaController.destroy);
 module.exports = router;
