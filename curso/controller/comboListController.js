052ed4ddb803f1af1a06e6e7f5c3eddd76715816
andreLagacione <lagacioneandre@gmail.com>
2020-03-02 22:22:46 -0300

    refactor to use global comboList build

--- curso/controller/comboListController.js
+++ curso/controller/comboListController.js
@@ -1,19 +1,10 @@
 const db = require('../../mongo-config');
 const schema = require('../schema');
 const model = db.model('cursos', schema, 'cursos');
+const { buildComboList } = require('../../commons/build-combo-list');
 
 module.exports = {
     async index(request, response) {
-        const cursos = await model.find({}).sort([['nome', 'asc']]).lean().exec();
-        let comboList = [];
-
-        cursos.map(item => {
-            comboList.push({
-                nome: item.name,
-                id: item._id
-            });
-        });
-
-        response.json(comboList);
+        return await buildComboList(response, model, 'nome');
     }
 }
--- materia/controller/comboListController.js
+++ materia/controller/comboListController.js
@@ -1,11 +1,10 @@
 const db = require('../../mongo-config');
 const schema = require('../schema');
 const model = db.model('materias', schema, 'materias');
-const { convertId } = require('../../commons/convert-id');
+const { buildComboList } = require('../../commons/build-combo-list');
 
 module.exports = {
     async index(request, response) {
-        const materias = await model.find({}).sort([['name', 'asc']]).lean().exec();
-        response.json(convertId(materias));
+        return await buildComboList(response, model, 'name');
     }
 }
