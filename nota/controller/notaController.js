122bc621b5311520380bc5af0fc816d660973a37
andreLagacione <lagacioneandre@gmail.com>
2020-03-02 21:16:36 -0300

    create method to remove nota

--- nota/controller/notaController.js
+++ nota/controller/notaController.js
@@ -118,6 +118,30 @@ module.exports = {
             message: 'Nota alterada com sucesso!'
         });
     },
+
+    async destroy(request, response) {
+        const _id = request.params.id;
+
+        const _response = await model.deleteOne({
+            '_id': new ObjectId(_id)
+        });
+
+        if (_response && _response.deletedCount === 0) {
+            response.status(404).send({
+                httpStatus: 'Not Found',
+                httpStatusCode: 404,
+                message: 'Nota não encontrada!'
+            });
+
+            return false;
+        }
+
+        response.json({
+            httpStatus: 'OK',
+            httpStatusCode: 200,
+            message: 'Nota removida com sucesso!'
+        });
+    }
 };
 
 const notasDTO = async notas => {
