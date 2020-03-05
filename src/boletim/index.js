74f82cd53f778b979b2ebd3a0de5e7051dfc70ee
andreLagacione <lagacioneandre@gmail.com>
2020-03-04 22:28:11 -0300

    create endpoint to generate boletim

--- src/boletim/index.js
+++ src/boletim/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const boletimController = require('./controller/boletimController');
+const reportController = require('./controller/reportController');
 
 router.post('/list', boletimController.index);
 router.post('/', boletimController.store);
+router.get('/gerar/:id', reportController.generate);
 router.get('/:id', boletimController.show);
 router.put('/', boletimController.update);
 router.delete('/:id', boletimController.destroy);
