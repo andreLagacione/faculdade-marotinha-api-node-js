8e99e9d282533007e63a71e901c8b2092aec51ae
andreLagacione <lagacioneandre@gmail.com>
2020-03-10 21:17:38 -0300

    remove test for turma comboList

--- src/turma/controller/comboList.spec.js
+++ src/turma/controller/comboList.spec.js
@@ -5,8 +5,8 @@ chai.use(sinonChai);
 
 global['fetch'] = require('node-fetch');
 
-describe('Turma Teste', () => {
-    it('Should return true', (done) => {
-        setImmediate(done);
-    })
-});
+// describe('Turma Teste', () => {
+//     it('Should return true', (done) => {
+//         setImmediate(done);
+//     })
+// });
