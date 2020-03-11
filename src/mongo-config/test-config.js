71e38630cbd3278b6a7dcfe4ef0cbcffac56d074
andreLagacione <lagacioneandre@gmail.com>
2020-03-10 21:17:07 -0300

    configure base test

--- src/mongo-config/test-config.js
+++ src/mongo-config/test-config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-let testSchema = new mongoose.Schema({}, {
-    collection: 'test'
-});
+let testSchema;
+let model;
+let Test;
 
 mongoose.connect(
     'mongodb://localhost:27017/faculdade_marotinha_test',
@@ -11,25 +11,29 @@ mongoose.connect(
     }
 );
 
-const model = mongoose.model('test', testSchema, 'test', true);
-
 const setSchema = schema => {
     testSchema = new mongoose.Schema(schema, {
         collection: 'test'
     });
-}
 
-const Test = mongoose.model('test', testSchema);
+    model = mongoose.model('test', testSchema, 'test', true);
+    Test = mongoose.model('test', testSchema);
+}
 
 const initDb = async data => {
     const request = new model(data);
     return await request.save();
 }
 
+const dropBd = () => {
+    model.collection.drop();
+}
+
 
 module.exports = {
     setSchema,
     initDb,
+    dropBd,
     getTest: () => Test.find(),
     addTest: data => new Test(data).save(),
     deleteTest: id => Test.findByIdAndRemove(id)
