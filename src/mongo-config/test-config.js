19ce15fdb199d7f65693515167e47b4db91a98f6
andreLagacione <lagacioneandre@gmail.com>
2020-03-11 21:49:17 -0300

    change config for only configure tha data base

--- src/mongo-config/test-config.js
+++ src/mongo-config/test-config.js
@@ -1,8 +1,4 @@
 const mongoose = require('mongoose');
-let testSchema;
-let model;
-let Test;
-
 mongoose.connect(
     'mongodb://localhost:27017/faculdade_marotinha_test',
     {
@@ -11,30 +7,4 @@ mongoose.connect(
     }
 );
 
-const setSchema = schema => {
-    testSchema = new mongoose.Schema(schema, {
-        collection: 'test'
-    });
-
-    model = mongoose.model('test', testSchema, 'test', true);
-    Test = mongoose.model('test', testSchema);
-}
-
-const initDb = async data => {
-    const request = new model(data);
-    return await request.save();
-}
-
-const dropBd = () => {
-    model.collection.drop();
-}
-
-
-module.exports = {
-    setSchema,
-    initDb,
-    dropBd,
-    getTest: () => Test.find(),
-    addTest: data => new Test(data).save(),
-    deleteTest: id => Test.findByIdAndRemove(id)
-};
+module.exports = mongoose;
